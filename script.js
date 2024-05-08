4b5a92ce99a2ef380bd82f510f9770098d2a7def
Vanshusrivastava <156321461+Vanshusrivastava@users.noreply.github.com>
2024-05-08 14:34:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const textarea=document.querySelector('textarea');
-const counter=document.querySelector('.counter');
-
-
-function count(){
-    const text=textarea.value;
-    const length=textarea.value.length;
-    counter.innerText=`${length}`
-}
